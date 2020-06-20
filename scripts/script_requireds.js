775fd58b8e64b6ce105cbb3daa6456a21ba04ff1
Daniel <danieltutistar@gmail.com>
2020-06-20 02:31:47 -0500

    vistas login & registro

---
+++ scripts/script_requireds.js
@@ -0,0 +1,20 @@
+
+// cambiar los required 
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // obtener por clase
+        var forms = document.getElementsByClassName('needs-validation');
+        // prevenir submints
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
